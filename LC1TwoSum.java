7b9729271cceed7dda61f4d897f0722364ddf3b9
Shrinivas Pai Kane <shrinivaskane@gmail.com>
2024-12-19 01:43:34 -0500

    Corrected class name

--- LC1TwoSum.java
+++ LC1TwoSum.java
@@ -1,4 +1,4 @@
-public class TwoSum {
+public class LC1TwoSum {
     public int[] twoSum(int[] nums, int target) {
         Map<Integer, Integer> map = new HashMap<>();
         for(int i = 0; i < nums.length; i++) {
