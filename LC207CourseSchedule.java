28f68d00a2b0db760670cbed90a4d8f763bf9eb1
shrinivaskane <40660138+shrinivaskane@users.noreply.github.com>
2025-11-09 15:11:36 -0600

    Implement Topological Sort for course scheduling
    Refactored canFinish method to use Topological Sort instead of DFS for course scheduling.

--- LC207CourseSchedule.java
+++ LC207CourseSchedule.java
@@ -3,6 +3,7 @@ class LC207CourseSchedule {
     Map<Integer, List<Integer>> map = new HashMap<>();
     
     public boolean canFinish(int numCourses, int[][] prerequisites) {
+        /* DFS approach 
         for(int i = 0; i< numCourses; i++) {
             map.put(i, new ArrayList<>());
         }
@@ -13,6 +14,37 @@ class LC207CourseSchedule {
             if(!dfs(i)) return false;
         }
         return true;
+        */
+
+        /* Topological Sort (kahn's algorithm) */
+        int[] indegree = new int[numCourses];
+        List<List<Integer>> adj = new ArrayList<>();
+        Queue<Integer> queue = new LinkedList<>();
+        for(int i = 0; i< numCourses; i++) {
+            adj.add(new ArrayList<>());
+        }
+        for(int[] preq: prerequisites) {
+            adj.get(preq[1]).add(preq[0]);
+            indegree[preq[0]]++;
+        }
+        for(int i = 0; i< numCourses; i++) {
+            if(indegree[i] == 0) {
+                queue.add(i);
+            }
+        }
+
+        int finished = 0;
+        while(!queue.isEmpty()) {
+            finished++;
+            int course = queue.poll();
+            for(int preq: adj.get(course)) {
+                indegree[preq]--;
+                if(indegree[preq] == 0) queue.add(preq);
+            }
+        }
+
+        return finished == numCourses;
+
     }
 
     private boolean dfs(int course) {
