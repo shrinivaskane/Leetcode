3d781f91884e047ef7658cf17e8992d42af3fb16
shrinivaskane <40660138+shrinivaskane@users.noreply.github.com>
2026-05-30 20:39:10 -0500

    Implement majority element algorithm in Java

---
+++ LC169_MajorityElement.java
@@ -0,0 +1,13 @@
+class LC169_MajorityElement {
+    public int majorityElement(int[] nums) {
+       int count = 0;
+       Integer candidate = null;
+       for(int i: nums) {
+        if(count == 0) {
+            candidate = i;
+        }
+        count += candidate == i? +1: -1;
+       }
+       return candidate;
+    }
+}
